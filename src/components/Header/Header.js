a14231fa5cad31baf081921b0ac6ff99b956952b
Rony163 <minhajulislamrony43@gmail.com>
2021-10-04 01:04:24 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className="bg-info">
+            <h1 className="">Programming School</h1>
+            <nav className="nav-link">
+                <Link to="/home">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/services">Services</Link>
+            </nav>
         </div>
     );
 };
